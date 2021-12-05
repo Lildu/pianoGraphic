aa3e7f1b816a8953eb1be205fa2d9aaf415f7423
Lildu <lildci83@gmail.com>
2021-12-05 23:14:17 +0100

    Update Tableau1.js

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -11,26 +11,14 @@ class Tableau1 extends Phaser.Scene{
 
     create(){
 
-        this.piano=this.add.container(0,0);
+
 
         /**let touch=this.add.image(0,0,'key-1').setOrigin(0,0);
         this.piano.add(touch);*/
 
         /**let touch=this.add.tileSprite(0,340, 1920,200,'key-1').setOrigin(0,0);
         this.piano.add(touch);*/
-        this.pianopress=this.add.container(0,0);
-        let touch2=this.add.image(250,335,'key-2').setOrigin(0,0);
 
-
-        this.pianopress.add(touch2);
-        this.pianopress.visible=false;
-
-        let touch=this.add.image(250,335,'key-1').setOrigin(0,0);
-        this.piano.add(touch);
-        for(let t=0;t<=13;t++){
-            let touch1=this.add.image(touch.x+t*(touch.width),335,'key-1').setOrigin(0,0);
-            this.piano.add(touch1);
-        }
 
 
         this.initKeyboard();
@@ -44,7 +32,6 @@ class Tableau1 extends Phaser.Scene{
             switch (kevent.keyCode)
             {
                 case Phaser.Input.Keyboard.KeyCodes.Q:
-                    this.piano.visible=true;
                     break;
 
 
