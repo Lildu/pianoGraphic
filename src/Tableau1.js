61d3ba89cc5fc2fdf40137e38391432dbeada638
Lildu <lildci83@gmail.com>
2021-12-05 22:23:15 +0100

    Update Tableau1.js

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -7,44 +7,62 @@ class Tableau1 extends Phaser.Scene{
         this.load.image('key-'+t, 'assets/piano/key-'+t+'.png');
         }
     }
-
+    getFrames(prefix,length){
+        let frames=[];
+        for (let i=1;i<=length;i++){
+            frames.push({key: prefix+i});
+        }
+        return frames;
+    }
 
     create(){
 
-        this.piano=this.add.container(0,0);
 
         /**let touch=this.add.image(0,0,'key-1').setOrigin(0,0);
         this.piano.add(touch);*/
 
         /**let touch=this.add.tileSprite(0,340, 1920,200,'key-1').setOrigin(0,0);
-        this.piano.add(touch);*/
-        this.pianopress=this.add.container(0,0);
-        let touch2=this.add.image(250,335,'key-2').setOrigin(0,0);
-
-
-        this.pianopress.add(touch2);
-        this.pianopress.visible=false;
-
-        let touch=this.add.image(250,335,'key-1').setOrigin(0,0);
         this.piano.add(touch);
-        for(let t=0;t<=13;t++){
-            let touch1=this.add.image(touch.x+t*(touch.width),335,'key-1').setOrigin(0,0);
-            this.piano.add(touch1);
-        }
+        let vt= Math.random(1,28);
+        let vX= Math.random(1,1920);
+        let vY= Math.random(1,1050);*/
+        this.star=this.add.container(0,0);
+
+        let star1=this.add.image(0,0, 'star-1').setOrigin(0,0);
+        this.star.add(star1);
+
 
 
         this.initKeyboard();
-        this.cameras.main.setBounds(0, 0, 1920, 1050);
-
+        this.cameras.main.setBounds(50, 50, 1920, 1050);
+        this.star.scrollFactorX=0;
 
     }
+
     initKeyboard(){
         this.input.keyboard.on('keydown', function(kevent)
         {
+
             switch (kevent.keyCode)
             {
+
                 case Phaser.Input.Keyboard.KeyCodes.Q:
-                    this.piano.visible=true;
+
+                    break;
+
+                case Phaser.Input.Keyboard.KeyCodes.S:
+                case Phaser.Input.Keyboard.KeyCodes.D:
+                case Phaser.Input.Keyboard.KeyCodes.F:
+                case Phaser.Input.Keyboard.KeyCodes.G:
+                case Phaser.Input.Keyboard.KeyCodes.H:
+                case Phaser.Input.Keyboard.KeyCodes.J:
+                case Phaser.Input.Keyboard.KeyCodes.K:
+                case Phaser.Input.Keyboard.KeyCodes.L:
+                case Phaser.Input.Keyboard.KeyCodes.M:
+                case Phaser.Input.Keyboard.KeyCodes.Z:
+                case Phaser.Input.Keyboard.KeyCodes.E:
+                case Phaser.Input.Keyboard.KeyCodes.T:
+                case Phaser.Input.Keyboard.KeyCodes.Y:
                     break;
 
 
@@ -54,6 +72,21 @@ class Tableau1 extends Phaser.Scene{
         {
             switch (kevent.keyCode)
             {
+                case Phaser.Input.Keyboard.KeyCodes.Q:
+                case Phaser.Input.Keyboard.KeyCodes.S:
+                case Phaser.Input.Keyboard.KeyCodes.D:
+                case Phaser.Input.Keyboard.KeyCodes.F:
+                case Phaser.Input.Keyboard.KeyCodes.G:
+                case Phaser.Input.Keyboard.KeyCodes.H:
+                case Phaser.Input.Keyboard.KeyCodes.J:
+                case Phaser.Input.Keyboard.KeyCodes.K:
+                case Phaser.Input.Keyboard.KeyCodes.L:
+                case Phaser.Input.Keyboard.KeyCodes.M:
+                case Phaser.Input.Keyboard.KeyCodes.Z:
+                case Phaser.Input.Keyboard.KeyCodes.E:
+                case Phaser.Input.Keyboard.KeyCodes.T:
+                case Phaser.Input.Keyboard.KeyCodes.Y:
+                    break;
 
 
             }
