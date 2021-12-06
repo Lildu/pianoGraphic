ce5859988342af70bd70dbbcad136693f00d431b
Lildu <lildci83@gmail.com>
2021-12-06 15:54:45 +0100

    maj

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -15,9 +15,18 @@ class Tableau1 extends Phaser.Scene{
     }
 
     create(){
+        /**var vX = add.Math.random(0,1000);
+        var vY = add.Math.random(0,700);*/
 
-        this.piano=this.add.container(0,0);
-
+        //this.st = this.add.container(0,0);
+        this.star1=this.add.image(0,0,'star-1').setOrigin(0,0);
+        this.star1.setVisible(false)
+        this.star1.setRandomPosition(10,10)
+        this.star1.scale=0.1;
+        //this.st.add('star1');
+        //this.st.scale=1;
+        //this.st.setRandomPosition(10,10);
+        //this.st.visible = true;
         /**let touch=this.add.image(0,0,'key-1').setOrigin(0,0);
         this.piano.add(touch);*/
 
@@ -30,7 +39,7 @@ class Tableau1 extends Phaser.Scene{
         this.pianopress.add(touch2);
         this.pianopress.visible=false;
 
-        let touch=this.add.image(250,335,'key-1').setOrigin(0,0);
+        let touch=this.add.image(250,335,'key-1').setOrigin(0,0):
         this.piano.add(touch);
        /** for(let t=0;t<=13;t++){
             let touch1=this.add.image(touch.x+t*(touch.width),335,'key-1').setOrigin(0,0);
@@ -39,19 +48,19 @@ class Tableau1 extends Phaser.Scene{
 */
 
         this.initKeyboard();
-        this.cameras.main.setBounds(0, 0, 1920, 1080);
-
-
     }
+
+
     initKeyboard(){
+        let me = this;
         this.input.keyboard.on('keydown', function(kevent)
         {
             switch (kevent.keyCode)
             {
                 case Phaser.Input.Keyboard.KeyCodes.Q:
-
-                    break;
-
+                    me.star1.setVisible(true)
+                    console.log('japuis')
+                    break
                 case Phaser.Input.Keyboard.KeyCodes.S:
 
                     break;
@@ -64,14 +73,16 @@ class Tableau1 extends Phaser.Scene{
         {
             switch (kevent.keyCode)
             {
+                case Phaser.Input.Keyboard.KeyCodes.Q:
+                    me.star1.setVisible(true)
+                    console.log('je lache')
+                    break;
 
+                case Phaser.Input.Keyboard.KeyCodes.S:
+
+                    break;
 
             }
         });
-    }
-
-
-    update(){
-
     }
 }
